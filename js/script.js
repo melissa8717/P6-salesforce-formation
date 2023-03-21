745733aafec81e7207f7f93adf4f64a23268964b
melissa8717 <melgraph.design@gmail.com>
2023-03-21 16:54:09 +0100

    api calling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 function API(){
     
-    GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey;
+    //GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyDPkQzkp7tT8hwUHtCOMVg634fYz_qXMl4
+    var inputAuthor = document.getElementById("author").value;
+    var inputTitleBook = document.getElementById("title-book").value;
+
+    console.log(inputAuthor);
+
+    fetch("https://www.googleapis.com/books/v1/volumes?q="+inputTitleBook+"+inauthor:"+inputAuthor+"&key=AIzaSyDPkQzkp7tT8hwUHtCOMVg634fYz_qXMl4")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 
 }
